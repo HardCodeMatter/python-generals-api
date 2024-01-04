24d0937ff6f8291371230b7abe30ee6eb396a013
HardCodeMatter <dklymovets1@gmail.com>
2024-01-04 11:52:35 +0200

    Added Field default values and field_validator for username in User model.

--- src/dto/user.py
+++ src/dto/user.py
@@ -1,5 +1,17 @@
-from pydantic import BaseModel
+from pydantic import (
+    BaseModel, 
+    Field, 
+    field_validator
+)
 
 
 class User(BaseModel):
-    username: str
+    username: str = Field(min_length=3, max_length=20)
+
+    @field_validator('username')
+    @classmethod
+    def verify_username(cls, value: str) -> None:
+        if not value.isalnum():
+            raise ValueError('Username contains illegal characters.')
+         
+        return value.lower()
